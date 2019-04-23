fe1d5f1427bc118c300df568939e7bc176e3b683
BakerAO <dpshunter@gmail.com>
2019-04-23 14:02:08 -0500

    Start form

--- client/src/components/TravelForm.js
+++ client/src/components/TravelForm.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
 export default class TravelForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: '',
+            endDate: '',
+            policyMax: '',
+            citizenship: '',
+            age: '',
+            mailingState: ''
+        };
+    }
+
+    handlePolicyMaxChange = (event) => {
+        this.setState({ policyMax: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        // send form to back-end, then load results page
+    }
+
+
     render() {
         return (
             <div>
-                TravelForm
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Policy Maximum
+                    </label>
+                    <select value={this.state.policyMax} onChange={this.handlePolicyMaxChange}>
+                        <option value="50">$50,000</option>
+                        <option value="100">$100,000</option>
+                    </select>
+                </form>
+                <input type="submit" value="Submit" />
+                <button>
+                    Reset Form
+                </button>
             </div>
         );
     }
